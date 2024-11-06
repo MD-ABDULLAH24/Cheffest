2f53e6425ca79d0c88f48619f63c30259fce066f
Abdullah <abdullah.codingmind@gmail.com>
2024-11-06 14:17:54 +0600

    add all file and folder

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    fontFamily: {
+      'inter': ['inter', 'sans-serif'],
+    },
+    container: {
+      center: true,
+    },
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1202px',
+      // => @media (min-width: 1280px) { ... }
+    },
+    extend: {},
+  },
+  plugins: [],
+  darkMode: 'selector',
+}
+
